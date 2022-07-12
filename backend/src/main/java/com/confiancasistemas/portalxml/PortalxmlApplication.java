079662a0083b0222f485069be3e8670692b3a895
geroncio_rocha <geronciorocha@hotmail.com>
2022-07-12 00:08:32 -0300

    Project created

---
+++ backend/src/main/java/com/confiancasistemas/portalxml/PortalxmlApplication.java
@@ -0,0 +1,13 @@
+package com.confiancasistemas.portalxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalxmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalxmlApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/confiancasistemas/portalxml/PortalxmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confiancasistemas.portalxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalxmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
